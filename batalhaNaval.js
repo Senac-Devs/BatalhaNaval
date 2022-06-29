4bdf451d6163e3709b1c4baa2cfa1123fda7cbe4
Johnatan Mai <106967540+JohnatanMai@users.noreply.github.com>
2022-06-29 12:31:28 +0000

    modificacao de codigo com menos linhas

--- batalhaNaval.js
+++ batalhaNaval.js
@@ -2,8 +2,10 @@ let modelBombas = require ("./logicaBombas.js");
 
 
 function tipoNavios() {
-  let logica = require("./logicaDeVisualizacao.js");
-  logica.apresentaLogica();
+    let logica = require("./logicaDeVisualizacao.js");
+    let tabuleiro = logica.apresentaLogica();
+    console.log(tabuleiro)
+    return tabuleiro
 }
 
 function posicionar() {
@@ -12,27 +14,31 @@ function posicionar() {
 }
 
 function apresentacao() {
-  console.log("\n         ..................................." +
+    console.log("\n         ..................................." +
     "\n           ### Jogo de Batalha Naval ####" +
     "\n         ..................................." +
     "\n         Jogo de tabuleiro de dois jogadores" +
     "\nSeu objectivo é derrubar os barcos do oponente adversário," +
     "\nganha quem derrubar todos os navios adversários primeiro." +
     "\n")
-  console.log("!! Vamos Jogar !!")
-  console.log("Qualquer caracter = sim")
-  let jogar = prompt("Ou so enter para nao - ")
-  if (!jogar == "") {
-    console.clear()
-    tipoNavios()
-  } else console.log(" \n !! Que pena !!")
+    console.log("!! Vamos Jogar !!")
+    console.log("Qualquer caracter = sim")
+    let jogar = prompt("Ou so enter para nao - ")
+    let tabuleiro;
+    if (jogar != "") {
+        console.clear()
+        tabuleiro = tipoNavios()
+    } else {
+        console.log(" \n !! Que pena !!")
+    }
+    return tabuleiro
 }
 
   function iniciaJogo() {
-    apresentacao()
+    let tabuleiro = apresentacao()
     // posicionar()
-      
-    modelBombas.RodarBombas()    
+    tabuleiro[0][0]="<"
+    modelBombas.RodarBombas(tabuleiro)    
 
   }
   // modulo de chamada para o index.js
--- logicaBombas.js
+++ logicaBombas.js
@@ -3,64 +3,54 @@ module.exports = {
 }
 
 function bombas(tabuleiro, linha, coluna) {
-    let tab = [];
     let acertou = true;
-    if (tabuleiro[linha][coluna] == "-") {
+    if (tabuleiro[linha][coluna] == "~") {
         acertou = false
 
     } else { acertou = true }
-
-
-    var jogador1, jogador2;
-    //Jogador da rodada
-    var jogadorAtual;
-    var forma = "+";
-    jogadorAtual = forma;
-    bombas[linha][coluna] = barco
-    console.table(tabuleiro)
+    
     return acertou
+    
 }
-let tabuleiro = []
-for (let i = 0; i < 5; i++) {
-    tabuleiro.push([])
-    for (let j = 0; j < 5; j++) {
-        tabuleiro[i].push("-")
 
-    }
-}
-function RodarBombas () { 
+function RodarBombas (tabuleiro) { 
     console.table(tabuleiro)
     let linha = prompt("Digite a linha: ")
     let coluna = prompt("Digite a coluna:")
     
-    bombas(tabuleiro, linha, coluna);
-    
-    var navio1 = 2;
-    var navio2 = 3;
-    var navio3 = 4;
-    var jogada;
-    var acertou = 0;
-    var foiAfundado = false;
-    
-    while (!foiAfundado) {
-        jogada = prompt(bombas);
-        if (jogada) {
-            coordenadas == ~
-                console.log("Nenhuma embarcação atingida")
-        } else if (jogada) {
-            jogada == ">" || jogada == "<" || jogada == "=" || jogada == "="|| jogada == "v" ||jogada == "ʌ" ||
-                console.log("Parte do navio acertada!")
-            jogada = jogada + 1;
-        }
-    
-        if (jogada == navio1 || jogada == navio2 || jogada == navio3) {
-            alert("acerte mais um quadro!");
-            acertou = acertou + 1;
-            if (acertou == 3) {
-                foiAfundado = true;
-                alert("navio totalmente afundado!");
-            }
-    
-        }
+   let acertou = bombas(tabuleiro, linha, coluna);
+    if (acertou) {
+        console.log("Parte do navio acertada!")
+    } else {
+     console.log("Nenhuma embarcação atingida")
     }
 }
+    
+    // var navio1 = 2;
+    // var navio2 = 3;
+    // var navio3 = 4;
+    // var jogada;
+    // var acertou = 0;
+    // var foiAfundado = false;
+    
+    // while (!foiAfundado) {
+    //     jogada = prompt(bombas);
+    //     if (jogada) {
+    //         coordenadas == ~
+    //             console.log("Nenhuma embarcação atingida")
+    //     } else if (jogada) {
+    //         jogada == ">" || jogada == "<" || jogada == "=" || jogada == "="|| jogada == "v" ||jogada == "ʌ" ||
+    //             console.log("Parte do navio acertada!")
+    //         jogada = jogada + 1;
+    //     }
+    
+    //     if (jogada == navio1 || jogada == navio2 || jogada == navio3) {
+    //         alert("acerte mais um quadro!");
+    //         acertou = acertou + 1;
+    //         if (acertou == 3) {
+    //             foiAfundado = true;
+    //             alert("navio totalmente afundado!");
+    //         }
+    
+    //     }
+    // }
