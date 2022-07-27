724a04c9f6e502fd645fb0477b7c1836f4b69bf9
JoohDias <107855058+JoohDias@users.noreply.github.com>
2022-07-27 09:00:05 -0300

    Revert "Romavini teste #10"

--- batalhaNaval.js
+++ batalhaNaval.js
@@ -1,19 +1,41 @@
-const prompt = require("prompt-sync")();
 let modelBombas = require ("./logicaBombas.js");
-let logica = require("./logicaDeVisualizacao.js");
-let posicionar = require("./posicionarEmbarcaçoes.js");
+const prompt = require("prompt-sync")();
 
-function iniciaJogo() {
-  let querJogar = logica.apresentacao();
-  if (querJogar){
-    let tabuleiro = logica.geraTabuleiro()
-    posicionar.Coordenada(tabuleiro);
-    modelBombas.RodarBombas();    
+function tipoNavios() {
+  let logica = require("./logicaDeVisualizacao.js");
+  logica.apresentaLogica();
+}
+
+function posicionar() {
+  let posicionar = require("./posicionarEmbarcaçoes.js");
+  posicionar.posicionamento();
+}
+
+function apresentacao() {
+  console.log("\n         ..................................." +
+    "\n           ### Jogo de Batalha Naval ####" +
+    "\n         ..................................." +
+    "\n         Jogo de tabuleiro de dois jogadores" +
+    "\nSeu objectivo é derrubar os barcos do oponente adversário," +
+    "\nganha quem derrubar todos os navios adversários primeiro." +
+    "\n")
+  console.log("!! Vamos Jogar !!")
+  console.log("Qualquer caracter = sim")
+  let jogar = prompt("Ou so enter para nao")
+  if (!jogar == "") {
+    console.clear()
+    tipoNavios()
+  } else console.log(" \n !! Que pena !!")
+}
+
+  function iniciaJogo() {
+    apresentacao()
+    // posicionar()
+      
+    modelBombas.lancarBombas()    
+
   }
-}
-// modulo de chamada para o index.js
-module.exports = {
-  "exportadorIniciaJogo": iniciaJogo
-}
-logica.apresentaLogica();
-  
+  // modulo de chamada para o index.js
+  module.exports = {
+    "exportadorIniciaJogo": iniciaJogo
+  }
