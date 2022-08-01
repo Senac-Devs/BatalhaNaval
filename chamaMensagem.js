67abdd6ef3c212cf6858fed1101befa6a01ba2c3
Usuario Campo Mourao <uept11-user@aluno.pr.senac.br>
2022-08-01 09:05:31 -0300

    modificaçoes nas mensagens do jogo

---
+++ chamaMensagem.js
@@ -0,0 +1,21 @@
+function msginstrucao(){
+    console.log ("!")
+}
+
+
+function msgAcertou(){
+    console.log ("Acertou navio!")
+}
+
+
+function msgAgua(){
+    console.log ("Não acertou nada!")
+}
+
+function msgBomba(){
+    console.log ("Acertou a bomba!")
+}
+
+function msgVencer(){
+    console.log ("Parabéns você venceu!")
+}
